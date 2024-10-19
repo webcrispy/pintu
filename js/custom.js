a9932350c1b9983c90c6371bdd2e77c0ad3fb7cd
webcrispy <webcrispy7@gmail.com>
2024-10-19 15:49:37 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,129 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+   
+    nav:true,
+    autoplay:true,
+    autoplayspeed:1000,
+    items:1,
+    
+})
+
+
+
+// Show the first tab and hide the rest
+// Show the first tab and hide the rest
+$('.middle-tab ul li:first-child').addClass('active');
+$('.tab-content').hide();
+$('.tab-content:first-child').show();
+
+// Click function
+$('.middle-tab ul li').click(function(){
+  $('.middle-tab ul li').removeClass('active');
+  $(this).addClass('active');
+  $('.tab-content').hide();
+  
+  var activeTab = $(this).find('a').attr('href');
+  $(activeTab).fadeIn();
+  return false;
+});
+
+
+
+// Trigger CSS animations on scroll.
+// Detailed explanation can be found at http://www.bram.us/2013/11/20/scroll-animations/
+
+// Looking for a version that also reverses the animation when
+// elements scroll below the fold again?
+// --> Check https://codepen.io/bramus/pen/vKpjNP
+
+jQuery(function($) {
+  
+  // Function which adds the 'animated' class to any '.animatable' in view
+  var doAnimations = function() {
+    
+    // Calc current offset and get all animatables
+    var offset = $(window).scrollTop() + $(window).height(),
+        $animatables = $('.animatable');
+    
+    // Unbind scroll handler if we have no animatables
+    if ($animatables.length == 0) {
+      $(window).off('scroll', doAnimations);
+    }
+    
+    // Check all animatables and animate them if necessary
+        $animatables.each(function(i) {
+       var $animatable = $(this);
+            if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+        $animatable.removeClass('animatable').addClass('animated');
+
+        $('.counter').each(function() {
+  var $this = $(this),
+      countTo = $this.attr('data-count');
+  
+  $({ countNum: $this.text()}).animate({
+    countNum: countTo
+  },
+
+  {
+
+    duration: 8000,
+    easing:'linear',
+    step: function() {
+      $this.text(Math.floor(this.countNum));
+    },
+    complete: function() {
+      $this.text(this.countNum);
+      //alert('finished');
+    }
+
+  });  
+  
+  
+
+});
+            }
+    });
+
+    };
+  
+  // Hook doAnimations on scroll, and trigger a scroll
+    $(window).on('scroll', doAnimations);
+  $(window).trigger('scroll');
+
+});
+
+
+
+/* jQuery
+================================================== */
+$(function() {
+  $('.acc__title').click(function(j) {
+    
+    var dropDown = $(this).closest('.acc__card').find('.acc__panel');
+    $(this).closest('.acc').find('.acc__panel').not(dropDown).slideUp();
+    
+    if ($(this).hasClass('active')) {
+      $(this).removeClass('active');
+    } else {
+      $(this).closest('.acc').find('.acc__title.active').removeClass('active');
+      $(this).addClass('active');
+    }
+    
+    dropDown.stop(false, true).slideToggle();
+    j.preventDefault();
+  });
+});
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+
+                                    new Mmenu("#menu", {        theme       : "white",
+        navbars     : {
+            content : [ "prev", "title" ]
+        },
+    
+        setSelected : {
+            hover: true
+        }}, {});
+                
+                            });
